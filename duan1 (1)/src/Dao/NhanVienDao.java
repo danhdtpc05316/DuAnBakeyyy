d9c6db3b647fe2317a5bbdbe1eb512f35d94269f
quangtt-pc06371 <151135993+quangtt-pc06371@users.noreply.github.com>
2023-11-25 16:55:00 +0700

    suadao

--- duan1 (1)/src/Dao/NhanVienDao.java
+++ duan1 (1)/src/Dao/NhanVienDao.java
@@ -5,11 +5,43 @@
 package Dao;
 
 import entity.NhanVien;
+import java.util.List;
 
 /**
  *
  * @author MSI8
  */
-public class NhanVienDao extends duan1Dao <NhanVien, String> {
+public class NhanVienDao extends duan1Dao<NhanVien, String> {
+    String INSERT_SQL = "INSERT INTO NhanVien(id_NV,HoTenNV,MatKhau,VaiTro,GioiTinh,SDT,Email,DiaChi,Hinh) VALUES(?,?,?,?,?,?,?,?,?)";
+    String UPDATE_SQL = "UPDATE NhanVien SET MatKhau=?,HoTen=?,VaiTro=? WHERE MaNV=?";
+    String DELETE_SQL = "DELETE FROM NhanVien WHERE MaNV =?";
+    String SELECT_ALL_SQL = "SELECT*FROM NhanVien";
+    String SELECT_BY_ID_SQL = "SELECT*FROM NhanVien WHERE MaNV=?";
+    
+    
+    @Override
+    public void insert(NhanVien entity) {
+        
+    }
+
+    @Override
+    public void update(NhanVien entity) {
+    }
+
+    @Override
+    public void delete(String id) {
+    }    
+
+    @Override
+    public NhanVien selectById(String id) {
+    }   
+
+    @Override
+    public List<NhanVien> selectAll() {
+    }
+
+    @Override
+    protected List<NhanVien> selectBySql(String sql, Object... args) {
+    }
     
 }
